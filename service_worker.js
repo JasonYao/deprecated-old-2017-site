bd6dcc678b33feba974271b4b4ab004b087d9a3d
Jason Yao <jasony.edu@gmail.com>
2017-09-30 17:03:45 -0400

    Made the offline page pretty and showed up correctly when offline

--- service_worker.js
+++ service_worker.js
@@ -4,14 +4,43 @@
 
 let urlsToCache = [];
 
-// Cache post urls
+// Caches the offline url
+urlsToCache.push("{{ site.base_url | prepend: site.url }}/offline/");
+
+// Caches some required AMP libraries
+let amp_libraries = [
+    "https://cdn.ampproject.org/v0/amp-analytics-0.1.js",
+    "https://cdn.ampproject.org/v0/amp-iframe-0.1.js",
+    "https://cdn.ampproject.org/v0/amp-image-lightbox-0.1.js",
+    "https://cdn.ampproject.org/v0/amp-social-share-0.1.js",
+    "https://cdn.ampproject.org/v0/amp-sidebar-0.1.js",
+    "https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js",
+    "https://cdn.ampproject.org/v0/amp-accordion-0.1.js",
+    "https://cdn.ampproject.org/shadow-v0.js"
+];
+
+amp_libraries.forEach(function (amp_library) {
+    urlsToCache.push(amp_library);
+});
+
+// Cache latest 10 post urls
 {% for post in site.posts limit:10 %}
-    urlsToCache.push("{{ post.url }}");
+    urlsToCache.push("{{ post.url | prepend: site.base_url | prepend: site.url }}");
 {% endfor %}
 
-// Cache project urls
+// Caches latest 10 amp post urls
+{% for amp_post in site.amp_posts limit:10 %}
+    urlsToCache.push("{{ amp_post.url | prepend: site.base_url | prepend: site.url }}");
+{% endfor %}
+
+// Cache latest 10 project urls
 {% for project in site.projects limit:10 %}
-    urlsToCache.push("{{ project.url }}");
+    urlsToCache.push("{{ project.url | prepend: site.base_url | prepend: site.url }}");
+{% endfor %}
+
+// Cache latest 10 project urls
+{% for amp_project in site.amp_projects limit:10 %}
+    urlsToCache.push("{{ amp_project.url | prepend: site.base_url | prepend: site.url }}");
 {% endfor %}
 
 // To invalidate service worker caches, just increment the semver-compliant cache name
@@ -41,7 +70,7 @@ self.addEventListener('fetch', function(event) {
             });
         }).catch(function() {
             // Fallback to the offline page if not available in the cache.
-            return caches.match('/offline/');
+            return caches.match("{{ site.base_url | prepend: site.url }}/offline/");
         })
     );
 });
@@ -56,7 +85,7 @@ self.addEventListener('fetch', function(event) {
             });
         }).catch(function() {
             // Fallback to the offline page if not available in the cache.
-            return caches.match('/offline/');
+            return caches.match("{{ site.base_url | prepend: site.url }}/offline/");
         })
     );
 });
